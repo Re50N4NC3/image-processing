179430bd2063fa70095a831aed988680b22bb741
Re50N4NC3 <47196639+Re50N4NC3@users.noreply.github.com>
2022-09-24 00:16:55 +0200

    Spelling and comment fix

--- monochrome.py
+++ monochrome.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def rgb_to_mono(rgb_array):
 	try:
-		# return dot product of the array scaled by balancing values itu R 601 2 luma transform
+		# return dot product of the array scaled by balancing values using itu R 601 2 luma transform values
 		return np.dot(rgb_array[..., :3], [0.299, 0.587, 0.114]).astype(np.uint8)
 	except ValueError:
-		print("Provided argument is not valid 3 dimensional numpy array")
+		print("Provided argument is not valid, 3 dimensional numpy array")
