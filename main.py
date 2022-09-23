cb689f7736c4e98d2239d15b610fe31d95e23124
Re50N4NC3 <47196639+Re50N4NC3@users.noreply.github.com>
2022-09-24 00:16:06 +0200

    Added thresholding

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from PIL import Image
 from monochrome import rgb_to_mono
+import thresholding
 import numpy as np
+
 
 def open_image_as_array(file_name):
 	try:
 		img = Image.open(file_name)
 		return np.array(img)
-	except ValueError:
+	except FileNotFoundError:
 		print("File doesn't exist")
 		return
 
-def show_image(image_array, save_name='show.png'):
+def show_image_from_array(image_array, save_name='show.png'):
 	try:
 		img = Image.fromarray(image_array, 'RGB')
 	except ValueError:
@@ -20,6 +22,8 @@ def show_image(image_array, save_name='show.png'):
 	img.show()
 
 
-sample_image = open_image_as_array('test.jpg')
+sample_image = open_image_as_array('test.png')
 monochrome_image = rgb_to_mono(sample_image)
-show_image(monochrome_image)
+## show_image_from_array(monochrome_image)
+thrsh = thresholding.threshold_adaptive(monochrome_image)
+show_image_from_array(thrsh)
