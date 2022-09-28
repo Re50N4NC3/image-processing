3938a0eb6383336ccd92db25bd179c13f3bdf964
Re50N4NC3 <47196639+Re50N4NC3@users.noreply.github.com>
2022-09-28 19:49:40 +0200

    Added gaussian blur

--- blur.py
+++ blur.py
@@ -1,4 +1,5 @@
 import numpy as np
+import convolution
 
 
 def generate_gauss_kernel(size=5, sigma=1):
@@ -10,3 +11,13 @@ def generate_gauss_kernel(size=5, sigma=1):
           diff = np.sqrt((i - center)**2 + (j - center)**2)
           kernel[i,j] = np.exp(-(diff**2) / (2*sigma**2))
     return kernel/np.sum(kernel)
+
+def gaussian_blur(image_array, kernel_size=5, sigma=1):
+	if kernel_size > 8:
+		print("Kernel size too big, please keep it below 8")
+		return
+
+	kernel = generate_gauss_kernel(kernel_size, sigma)
+	blurred_image = convolution.conv_2d(image_array, kernel)
+	
+	return blurred_image
